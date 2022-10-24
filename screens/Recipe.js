4e72604d3ecf8d6de966520409f687ae590495e0
BlessingAdewale <adelekeblessingadewale@gmail.com>
2022-10-24 16:02:34 +0100

    Update Recipe.js

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -166,6 +166,126 @@ const Recipe = ({navigation, route}) => {
      }, [] )
 
 
+function renderHeaderBar() {
+    return (
+        <View
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                height: 90,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+                justifyContent: 'space-between',
+                paddingHorizontal: SIZES.padding,
+                paddingBottom: 10
+
+        }}
+        >
+
+        {/* Screen Overlay */}
+
+        <Animated.View
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: COLORS.black,
+                opacity: scrollY.interpolate ({
+                    inputRange: [HEADER_HEIGHT - 100, HEADER_HEIGHT - 70],
+                    outputRange: [0, 1]
+                })
+
+            }}
+        
+        />
+
+        {/* Header Bar Title */}
+        <Animated.View
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                paddingBottom: 10,
+                opacity: scrollY.interpolate({
+                    inputRange: [HEADER_HEIGHT -100, HEADER_HEIGHT - 50],
+                    outputRange: [0, 1]
+                }),
+
+                transform: [{
+                    translateY: scrollY.interpolate({
+                        inputRange: [HEADER_HEIGHT - 100, HEADER_HEIGHT - 50],
+                        outputRange: [50, 0],
+                        extrapolate: 'clamp'
+                    })
+                }]
+            }}
+            
+        >
+            <Text style={{color: COLORS.lightGray2, ...FONTS.body4}}>Recipe by:</Text>
+            <Text style={{color: COLORS.white2, ...FONTS.h3}}>{selectedRecipe?.author?.name}</Text>
+
+        </Animated.View>
+
+
+            {/* Back Button */}
+            <TouchableOpacity
+            style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 35,
+                width: 35,
+                borderRadius: 18,
+                borderWidth: 1,
+                borderColor: COLORS.lightGray,
+                backgroundColor: COLORS.transparentBlack5
+            }}
+
+            onPress={() => navigation.goBack()}
+
+            >
+                <Image
+                source={icons.back}
+                style={{
+                    width: 15,
+                    height: 15,
+                    tintColor: COLORS.lightGray
+                }}
+                
+                />
+            </TouchableOpacity>
+
+            {/* Bookmark */}
+
+            <TouchableOpacity
+            style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: 35,
+                width: 35
+            }}
+            >
+                <Image
+                    source={selectedRecipe?.isBookmark ? icons.bookmarkFilled : icons.bookmark}
+                    style={{
+                        width: 30,
+                        height: 30,
+                        tintColor: COLORS.darkGreen
+                    }}
+
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}     
+
 function renderRecipeCardHeader() {
 
     return(
@@ -212,7 +332,9 @@ function renderRecipeCardHeader() {
                 height: 80,
                 transform: [{
                     translateY: scrollY.interpolate({
-
+                        inputRange: [0, 170, 250],
+                        outputRange: [0, 0, 100],
+                        extrapolate: 'clamp'
                     })
                 }
                 ]
@@ -245,6 +367,7 @@ function renderRecipeCardHeader() {
                     {/* Header */}
                     {renderRecipeCardHeader()}
                     {/* Info */}
+                    {renderRecipeInfo()}
                     {/* {renderRecipeInfo()} */}
                     {/* Ingredient Title */}
                 </View>
@@ -323,6 +446,9 @@ function renderRecipeCardHeader() {
             
             )}
             />
+            {/* Header Bar */}
+
+            {renderHeaderBar()}
         </View>
     )
 }
